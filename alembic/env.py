d6f96eef527bf6b6dad1478a495ea78367700573
ambani elphas <ambanielphas@gmail.com>
2026-01-17 14:14:57 +0300

    Add Alembic init and revision commands comments
    Added comments for Alembic initialization and commands.

--- alembic/env.py
+++ alembic/env.py
@@ -1,4 +1,10 @@
+#alembic init alembic
 from database import Base
 from domain.entities import Item
 
 target_metadata = Base.metadata
+
+#alembic revision --autogenerate -m "create items table"
+#alembic upgrade head
+
+
